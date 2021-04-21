6def6337380d601fc229713ac2e6e8a553902a1a
broklyn777 <50321354+broklyn777@users.noreply.github.com>
2021-04-21 23:19:02 +0200

    format bilder ester

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,12 +1,5 @@
 const projectsData = [
-  {
-    title: 'A Search Engine',
-    description: `What is you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
+ 
   {
     title: 'The Time Machine',
     description: `Imagine being able to travel back in time or to the future. Simple turn the knob
@@ -15,6 +8,23 @@ const projectsData = [
     imgSrc: '/static/images/time-machine.jpg',
     href: '/blog/the-time-machine',
   },
+   {
+    title: 'Ester Pysslar',
+    description: `Tänk dig att kunna resa med pysslet tillbaka i tiden eller till framtiden. Vrid vredet enkelt
+     till önskat datum och tryck på "Gå". Inte mer oroa dig för förlorade nycklar eller
+     glömda mobiltelefoner med denna enkla men prisvärda lösnings.`,
+    imgSrc: '/static/images/ester-pyssel1.jpeg',
+    href: '/blog/ester-pysslar',
+  },
+   {
+    title: 'A Search Engine',
+    description: `What is you could look up any information in the world? Webpages, images, videos
+    and more. Google has many features to help you find exactly what you're looking
+    for.`,
+    imgSrc: '/static/images/google.png',
+    href: 'https://www.google.com',
+  },
+  
 ]
 
 export default projectsData
