02818e4cdce056c7ba3bbb08480623602e171dbf
broklyn777 <50321354+broklyn777@users.noreply.github.com>
2021-04-23 14:52:27 +0200

    texter

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,13 +1,13 @@
 const projectsData = [
   {
     title: 'Gungställning',
-    description: `Denna titt på fyra sensoriska fördelar med lövlek, och höstens utomhusaktivitetsidé för familjen, får vi av vår gästbloggare Dagge.`,
+    description: `Denna titt på två otroliga fördelar med en gunga, och höstens utomhusaktivitetsidé för familjen, får vi av vår gästbloggare Dagge.`,
     imgSrc: '/static/images/he-gungar.jpeg',
     href: '/blog/lek-med-lov',
   },
   {
     title: 'Lek Med Löv',
-    description: `Denna titt på fyra sensoriska fördelar med lövlek, och höstens utomhusaktivitetsidé för familjen, får vi av vår gästbloggare Dagge.`,
+    description: `Löv!  Känn hur våren är på ingång med denna stora Lövhög  i ljusa och glada färger! Denna titt på fyra sensoriska fördelar.`,
     imgSrc: '/static/images/lek-med-lov.jpeg',
     href: '/blog/lek-med-lov',
   },
@@ -36,7 +36,7 @@ const projectsData = [
   },
   {
     title: 'Playdoh ',
-    description: `Lera!  Känn hur våren är på ingång med detta stora Play-Doh 12-pack i ljusa och glada färger!h-e-gungar`,
+    description: `Lera!  Känn hur våren är på ingång med detta stora Play-Doh 12-pack i ljusa och glada färger!`,
     imgSrc: '/static/images/playdo-lera.jpeg',
     href: '/blog/ester-pysslar',
   },
